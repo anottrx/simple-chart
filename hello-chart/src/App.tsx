afa6aee87900b7f9d8d88d7e19caea81945d606b
anottrx <anottrx@gmail.com>
2022-03-07 14:57:22 +0900

    feat : react-chartjs-2 Doughnut Chart Example

--- hello-chart/src/App.tsx
+++ hello-chart/src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const data = {
+  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: ["red", "blue", "yellow", "green", "purple", "orange"],
+      borderColor: ["red", "blue", "yellow", "green", "purple", "orange"],
+      borderWidth: 1,
+    },
+  ],
+};
+
+export function App() {
+  return <Doughnut data={data} />;
 }
-
-export default App;
