234423b72a569dcc851e5e937a08e3ae2eb5e375
ryuna <anottrx@gmail.com>
2022-03-08 09:36:16 +0900

    style : 코드 prettier 적용

--- hello-chart/src/App.tsx
+++ hello-chart/src/App.tsx
@@ -1,29 +1,31 @@
 import React, { useRef } from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut, getElementAtEvent } from "react-chartjs-2";
-import {DOUGHNUT_COLOR} from "./data/doughnutColor"
+import { DOUGHNUT_COLOR } from "./data/doughnutColor";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export const data = {
-  labels: DOUGHNUT_COLOR.map(position => position.labelName),
+  labels: DOUGHNUT_COLOR.map((position) => position.labelName),
   datasets: [
     {
       label: "# of Votes",
-      data: DOUGHNUT_COLOR.map(position => position.data),
-      backgroundColor: DOUGHNUT_COLOR.map(position => position.backgroundColor),
-      borderColor: DOUGHNUT_COLOR.map(position => position.borderColor),
+      data: DOUGHNUT_COLOR.map((position) => position.data),
+      backgroundColor: DOUGHNUT_COLOR.map(
+        (position) => position.backgroundColor
+      ),
+      borderColor: DOUGHNUT_COLOR.map((position) => position.borderColor),
       borderWidth: 1,
     },
   ],
 };
 
 export function App() {
-const chartRef: any = useRef(null);
+  const chartRef = useRef<any>();
 
-const onClick=(event: React.MouseEvent<HTMLCanvasElement, MouseEvent>)=> {
-  alert(data.labels[getElementAtEvent((chartRef.current), event)[0].index])
-}
+  const onClick = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+    alert(data.labels[getElementAtEvent(chartRef.current, event)[0].index]);
+  };
 
   return <Doughnut data={data} ref={chartRef} onClick={onClick} />;
 }
