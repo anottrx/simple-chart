c51dee49326bd431b6a3265174631fb5b70d1d31
anottrx <anottrx@gmail.com>
2022-03-10 09:27:19 +0900

    set : 인덱스 페이지 설정

--- chart-project/src/App.tsx
+++ chart-project/src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import IndexPage from "./pages/IndexPage";
 
 function App() {
-  return <div className="App">chart project</div>;
+  return (
+    <BrowserRouter>
+      <Route exact path="/" component={IndexPage} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
